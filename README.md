e230764233e5e3cd9a0fd2472c1a16ed073df22d
simonmonk <srmonk@gmail.com>
2025-04-12 17:47:01 +0100

    added Arduino test program

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ The design files and firmware for my project to build an FM broadcast receiver w
 
 At present, there is just a design for a PCB FM Antenna copied from this [research publication](https://www.researchgate.net/publication/277016433_A_Compact_Printed_Spiral_FM_Antenna)
 
+Good instructions and code here for [getting the RDA5807M FM Receiver working with Arduino.](https://www.instructables.com/Arduino-Based-Digital-Audio-FM-Radio-Using-RDA5807/)
+
 I have yet to try it out. Watch out for updates.
+
---
+++ software/TEA5767Test/TEA5767Test.ino
@@ -0,0 +1,55 @@
+/*
+ * Modified from original library and examples by big12boy 2017
+ * https://github.com/big12boy/TEA5767
+ * 
+ * Mostly reformat and recomment and simplification.
+ * 
+ * Signal strength only updates if you do a reset. Use Arduino Reset button
+ * 
+ * Tested on Arduino Uno connected to TEA5767 module. 
+ * 
+ * This used to test TEA5767 basic operation and measure signal strength for various antenna.
+ * 
+ * See blog post here for more detail: 
+ * https://www.doctormonk.com/2025/04/accessible-fm-radio-case-study-part-1.html
+ * 
+ */
+
+#include <TEA5767.h>
+TEA5767 radio = TEA5767();
+
+float frequency = 93.0; // Enter your own Frequency in MHz. Look up the frequency of a station near you. 
+long baud = 9600;       //Enter your own Baudrate. I always use 9600.
+
+void setup() {
+  Serial.begin(baud);
+  Wire.begin();
+  radio.setFrequency(frequency);
+}
+
+void loop() {
+  printReady();
+  printStereo();
+  printSignalLevel();
+  
+  Serial.println();
+  delay(1000); 
+}
+
+void printReady(){
+  int rdy = radio.getReady();
+  Serial.print("Ready: ");
+  Serial.println(rdy);
+}
+
+void printStereo(){
+  bool stereo = radio.isStereo(); 
+  Serial.print("Stereo: ");
+  Serial.println(stereo);
+}
+
+void printSignalLevel(){
+  short level = radio.getSignalLevel(); 
+  Serial.print("Signal (0-15): ");
+  Serial.println(level);
+}
